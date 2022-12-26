e80394967a22f533336297ba6a33a363067d2ed4
L2HYUNN <donghyuni98@gmail.com>
2022-12-26 18:30:45 +0900

    feat: add mocking cat data

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import * as express from "express";
+import { Cat } from "app.model";
 
 const app: express.Express = express();
 
 const port: number = 8000;
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
-  res.send("Hello World!");
+  res.send({ cats: Cat });
 });
 
 app.listen(port, () => {
