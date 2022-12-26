b1c8049177a0b28a03a49946558716c3aaad3ae9
L2HYUNN <donghyuni98@gmail.com>
2022-12-27 00:14:08 +0900

    feat: add create & read API

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,74 @@
 import * as express from "express";
-import { ExpressNextTypes, ExpressTypes } from "types";
 import { Cat } from "./app.model";
 
 const app: express.Express = express();
 
 const port: number = 8000;
 
-app.use(({ req, res, next }: ExpressNextTypes) => {
+//* logging middleware
+app.use((req, res, next: express.NextFunction) => {
   console.log(req.rawHeaders[1]);
   console.log("This is Logging Middleware");
   next();
 });
 
-app.get("/", ({ req, res }: ExpressTypes) => {
-  res.send({ cats: Cat });
+//* json middleware
+app.use(express.json());
+
+//* READ 전체 고양이 데이터 조회
+app.get("/cats", (req: express.Request, res: express.Response) => {
+  try {
+    const cats = Cat;
+    res.status(200).send({
+      sucess: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      sucess: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get("/cats/blue", ({ req, res }: ExpressTypes) => {
-  res.send({ blue: Cat[0] });
+//* READ 특정 고양이 데이터 조회
+app.get("/cats/:id", (req: express.Request, res: express.Response) => {
+  try {
+    const params = req.params;
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({
+      sucess: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      sucess: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get("/cats/som", ({ req, res }: ExpressTypes) => {
-  res.send({ blue: Cat[1] });
+// * CREATE 새로운 고양이 추가
+app.post("/cats", (req: express.Request, res: express.Response) => {
+  try {
+    const data = req.body;
+    Cat.push(data);
+  } catch (error: any) {
+    res.status(400).send({
+      sucess: false,
+      error: error.message,
+    });
+  }
 });
 
-app.use(({ req, res }: ExpressTypes) => {
+//* 404 middleware
+app.use((req: express.Request, res: express.Response) => {
   console.log("This is Logging Middleware");
   res.send({ error: "404 not found" });
 });
